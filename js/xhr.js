358755cf458055b8c2cbc3fc63690afa5cdff801
errinjohnson <errin.johnson@gmail.com>
2017-03-05 11:50:35 -0500

    refactoring code

--- js/xhr.js
+++ js/xhr.js
@@ -1,4 +1,44 @@
-        var settings = {
+var data = null;
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+        console.log(this.responseText);
+    }
+});
+
+xhr.open("GET", "https://louisvilleky.gov/services/toolbox_forms.json?node_title=");
+xhr.setRequestHeader("cache-control", "no-cache");
+
+xhr.send(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var settings = {
             "async": true,
             /*"crossDomain": true,*/
             "Content-Type": "application/json",
